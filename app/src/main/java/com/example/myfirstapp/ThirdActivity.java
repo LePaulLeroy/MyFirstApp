f6043d2b0a83741a36ac35f861f764760418c7a2
LePaulLeroy <pleroy@et.esiea.fr>
2019-03-08 17:59:39 +0100

    Nouvelle interface d'accueil

--- app/src/main/java/com/example/myfirstapp/ThirdActivity.java
+++ app/src/main/java/com/example/myfirstapp/ThirdActivity.java
@@ -3,25 +3,102 @@ package com.example.myfirstapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.myfirstapp.model.Competition;
 import com.google.gson.Gson;
-
+import com.squareup.picasso.Picasso;
 
 
 public class ThirdActivity extends Activity {
 
     private Competition data;
+    public TextView nomDeTeam;
+    public TextView nomDeLigue;
+    public TextView nomDeManager;
+    public TextView nomDeStade;
+    public TextView nomDeStadecapa;
+    public TextView nomDeStadeloca;
+    public ImageView ban;
+    public ImageView iconclub;
+    public ImageView stade;
+    public ImageView jersey;
+
 
 
     public void onCreate (Bundle savedInstanceState){
-
+        super.onCreate(savedInstanceState);
         Gson gson = new Gson();
         Intent intent = getIntent();
         data = gson.fromJson(intent.getStringExtra("data"),Competition.class);
 
+        ban = findViewById(R.id.ban);
+        iconclub = findViewById(R.id.icon);
+        stade = findViewById(R.id.stadium_photo);
+        jersey = findViewById(R.id.jersey_photo);
+        nomDeTeam = findViewById(R.id.team);
+        nomDeLigue = findViewById(R.id.ligue_name);
+        nomDeManager = findViewById(R.id.manager_name);
+        nomDeStade = findViewById(R.id.stadium_name);
+        nomDeStadecapa = findViewById(R.id.stadium_capacity);
+        nomDeStadeloca = findViewById(R.id.stadium_location);
+
+        final String name = data.getStrTeam();
+        final String compet = data.getStrLeague();
+        final String manager = data.getStrManager();
+        final int capa = data.getIntStadiumCapacity();
+        final String capas = Integer.toString(capa);
+        final String loca = data.getStrStadiumLocation();
+        final String stadium = data.getStrStadium();
+
+
+        //System.out.println("TTTTEEEESSSTTTT" + name);
+        nomDeTeam.setText(name);
+        nomDeLigue.setText(compet);
+        nomDeStadeloca.setText(loca);
+        nomDeStadecapa.setText(capas);
+        nomDeStade.setText(stadium);
+        nomDeManager.setText(manager);
+
+        //holder.txtHeader.setOnClickListener(new OnClickListener() {
+
+
+
+        Picasso
+                .get()
+                .load(data.getStrTeamBadge())
+                .placeholder(R.drawable.ic_launcher_background)
+                .resize(0,180)
+                .into(iconclub);
+
+        Picasso
+                .get()
+                .load(data.getStrTeamBanner())
+                .placeholder(R.drawable.ic_launcher_background)
+                .resize(0,180)
+                .into(ban);
+        Picasso
+                .get()
+                .load(data.getStrStadiumThumb())
+                .placeholder(R.drawable.ic_launcher_background)
+                .resize(0,180)
+                .into(stade);
+        Picasso
+                .get()
+                .load(data.getStrTeamJersey())
+                .placeholder(R.drawable.ic_launcher_background)
+                .resize(0,180)
+                .into(jersey);
+
+
+
     }
+
+
 
 
 }
